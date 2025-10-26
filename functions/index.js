db4141a20b6e03ea153e2e17676a2637eebae390
koshal <mrbiswas52527@gmail.com>
2025-10-26 09:41:27 +0530

    Delete functions directory

--- functions/index.js
+++
@@ -1,6 +0,0 @@
-// functions/index.js
-const functions = require('firebase-functions');
-
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hello from Firebase!");
-});
